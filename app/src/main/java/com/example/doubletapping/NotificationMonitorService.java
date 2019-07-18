2b2384ee7a452958e66941db64454847cd63c139
Young Newfield <43328428+jiyu596@users.noreply.github.com>
2019-07-18 18:30:48 +0800

    This is a fixer... Sorry for delay... :(

--- app/src/main/java/com/example/doubletapping/NotificationMonitorService.java
+++ app/src/main/java/com/example/doubletapping/NotificationMonitorService.java
@@ -3,25 +3,18 @@ package com.example.doubletapping;
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Bundle;
 import android.os.SystemClock;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import android.util.Log;
 import android.widget.Toast;
-
 
 
 @SuppressLint("NewApi")
 public class NotificationMonitorService extends NotificationListenerService {
     private AlarmManager alarmManager;
     private PendingIntent pendingIntent;
-    private boolean isqueuing = false;
+    private boolean isQueuing = false;
 
     @Override
     public void onCreate() {
@@ -31,21 +24,21 @@ public class NotificationMonitorService extends NotificationListenerService {
     // 在收到消息时触发
     @Override
     public void onNotificationPosted(StatusBarNotification statusBarNotification) {
-        // Toast.makeText(getApplicationContext(),"25",Toast.LENGTH_LONG).show();
-        if(isqueuing)
+        Toast.makeText(getApplicationContext(),"Got a notification!",Toast.LENGTH_LONG).show();
+        if(isQueuing)
             alarmManager.cancel(pendingIntent);
         Intent intent = new Intent(getApplication().getApplicationContext(),PlayNotificationSoundService.class);
         pendingIntent = PendingIntent.getService(getApplicationContext(),0,intent,0);
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime()+1000*3,pendingIntent);
-        isqueuing = true;
+        isQueuing = true;
     }
 
     // 在删除消息时触发
     @Override
     public void onNotificationRemoved(StatusBarNotification statusBarNotification) {
-        if(isqueuing) {
+        if(isQueuing) {
             alarmManager.cancel(pendingIntent);
         }
-        isqueuing = false;
+        isQueuing = false;
     }
 }
